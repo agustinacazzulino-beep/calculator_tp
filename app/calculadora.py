795c08fc961786fb8dec8a0902fa93fd9670424c
agustinacazzulino-beep <agustina.cazzulino@gmail.com>
2026-04-25 17:00:56 -0300

    feat: agrego calculadora.py con funciones correctas

---
+++ app/calculadora.py
@@ -0,0 +1,18 @@
+"""
+Modulo de calculadora basica.
+"""
+ 
+def sumar(a, b):
+    return a + b
+ 
+def restar(a, b):
+    return a - b
+ 
+def multiplicar(a, b):
+    return a * b
+ 
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir por cero")
+    return a / b
+
